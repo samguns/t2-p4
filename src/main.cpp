3738ec9778b1944d3fe13d9006ece6b3de84fc93
Gang Wang <gang.wang@broadcom.com>
2018-06-28 15:26:37 +0800

    writeup.

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main()
   pid.Init(0.23, 0.002, 20);
 
   Twiddle twiddle;
-  twiddle.Init();
+//  twiddle.Init();
 
   h.onMessage([&pid, &twiddle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -63,37 +63,43 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          std::ifstream params_file;
-          params_file.open("params.csv");
-          std::string Kp_str, Ki_str, Kd_str, reset;
-          double Kp, Ki, Kd;
-          while (params_file.good()) {
-            std::getline(params_file, Kp_str, ',');
-            std::getline(params_file, Ki_str, ',');
-            std::getline(params_file, Kd_str, ',');
-            std::getline(params_file, reset, ',');
-          }
-          Kp = std::stod(Kp_str);
-          Ki = std::stod(Ki_str);
-          Kd = std::stod(Kd_str);
-          if (0 == reset.compare("1")) {
-            pid.Init(Kp, Ki, Kd);
-            std::string msg = "42[\"reset\",{}]";
-            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-          }
+//          std::ifstream params_file;
+//          params_file.open("params.csv");
+//          std::string Kp_str, Ki_str, Kd_str, reset;
+//          double Kp, Ki, Kd;
+//          while (params_file.good()) {
+//            std::getline(params_file, Kp_str, ',');
+//            std::getline(params_file, Ki_str, ',');
+//            std::getline(params_file, Kd_str, ',');
+//            std::getline(params_file, reset, ',');
+//          }
+//          Kp = std::stod(Kp_str);
+//          Ki = std::stod(Ki_str);
+//          Kd = std::stod(Kd_str);
+//          if (0 == reset.compare("1")) {
+//            pid.Init(Kp, Ki, Kd);
+//            std::string msg = "42[\"reset\",{}]";
+//            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+//          }
 
-//          pid.UpdateError(cte);
-//          steer_value = -pid.TotalError();
-          twiddle.Process(ws, cte, 0.3);
+          pid.UpdateError(cte);
+          steer_value = -pid.TotalError();
+          if (steer_value < -1.0) {
+            steer_value = -1.0;
+          }
+          if (steer_value > 1.0) {
+            steer_value = 1.0;
+          }
+//          twiddle.Process(ws, cte, 0.3);
           
           // DEBUG
-//          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
-//
-//          json msgJson;
-//          msgJson["steering_angle"] = steer_value;
-//          msgJson["throttle"] = 0.3;
-//          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-//          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+
+          json msgJson;
+          msgJson["steering_angle"] = steer_value;
+          msgJson["throttle"] = 0.3;
+          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
         // Manual driving
